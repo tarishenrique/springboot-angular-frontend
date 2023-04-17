66bf73d50f5913dd40166db7b3065d7ec8f45b82
Taris Henrique <taris.tj@hotmail.com>
2023-04-17 17:04:42 -0300

    criacao de componente de lista de empregados no angular 1h11m23s

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent {
+
+  employees: Employee[];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
