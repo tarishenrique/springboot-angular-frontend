2892409460c1551a53996ea1a950c5fe20b41fc5
Taris Henrique <taris.tj@hotmail.com>
2023-05-09 22:25:56 -0300

    formulario para salvar empregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
@@ -16,6 +16,7 @@ import { CreateEmployeeComponent } from './create-employee/create-employee.compo
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
-export class CreateEmployeeComponent {
+export class CreateEmployeeComponent implements OnInit{
+
+  employee: Employee = new Employee();
+
+  constructor() { };
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit(){
+    console.log(this.employee);
+  }
+
+
 
 }
